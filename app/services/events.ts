ae7188399806bff8f774aba985990ca15e69ecce
Lior Shefer <liorshe@wix.com>
2025-10-31 13:16:08 +0200

    add cache: 'no-store'

--- app/services/events.ts
+++ app/services/events.ts
@@ -3,11 +3,11 @@ import { SPORT, LEAGUE, TEAM_ID } from "@/app/utils/consts";
 function EventsService() {
   const getEvents = async () => {
     const SCHEDULE_URL = `https://site.api.espn.com/apis/site/v2/sports/${SPORT}/${LEAGUE}/teams/${TEAM_ID}/schedule`;
-    const response = await fetch(SCHEDULE_URL);
+    const response = await fetch(SCHEDULE_URL, { cache: 'no-store' });
     return response.json();
   };
 
-  return {
+  return {    
     getEvents,
   };
 }
